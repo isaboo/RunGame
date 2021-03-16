443e9d524eeb8ea9e8fab3b198251bd4d79dcc0f
Isaboo <76838845+isaboo@users.noreply.github.com>
2021-03-16 15:02:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -56,9 +56,9 @@ var canvas = document.getElementById("canvas");
                     return;
                 }
                 var previousTile = this.floorTiles[this.floorTiles.length - 1];
-                var biggestJumpableHeight = previousTile.height + player.height + 90;
+                var biggestJumpableHeight = previousTile.height + player.height + 76;
                 if(biggestJumpableHeight > this.highestFloor) {
-                    biggestJumpableHeight = -50;
+                    biggestJumpableHeight = this.highestFloor;
                 }
                 var lowest = player.height + 50;
                 var randomHeight = Math.floor(Math.random() * biggestJumpableHeight) + lowest;
